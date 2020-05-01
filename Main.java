5d5706ca4326319b825a2950b0191e8ef333f5b7
peterlamphere <peter.lamphere@gmail.com>
2020-05-01 13:44:51 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+// MOCK AP Exam Answers
+/* Question 1: 
+https://kyap2020.com/wp-content/uploads/2020/04/AP-CSA-FRQ1.pdf
+
+Both answers in the Trip.java file
+
+Question 2: https://kyap2020.com/wp-content/uploads/2020/04/AP-CSA-FRQ2.pdf
+
+Answer below in the isSelfDivisor method
+*/
+
 class Main {
   public static void main(String[] args) {
     
+    // Client test code to print the numbers less than 200
+    // that are self divisors
     for (int i = 0; i < 200; i++) 
       System.out.println("Is "+ i + " a self divisor?" + isSelfDivisor(i));
       
@@ -17,19 +30,34 @@ class Main {
 
   }
 
+  // **** Question 2 solution ****
+
   public static boolean isSelfDivisor (int number) {
+    // Create a holding variable to store the number
     int current_number = number;
-    while (current_number > 0) { 
-      int lastDigit = current_number % 10;
-      if (lastDigit == 0 || (number % lastDigit != 0)) {
+
+    // Check to make sure the current number is bigger than zero
+    // This make sure we go through all the digits
+    while (current_number > 0) { // 1pt
+
+      // Get the last digit by taking the modulo of ten e.g. 128 % 10 = 8
+      int lastDigit = current_number % 10;  // 1 pt
+
+      // If the digit is zero, or the original number is not divisible by the digit
+      // we're done and return false 
+      if (lastDigit == 0 || (number % lastDigit != 0)) { // 1pt
         return false;
       }
-      // Faster: current_number /= 10;
-      current_number = current_number / 10;
-    }
-    return true;
-  }
-/*
 
-  */
+      // Truncate the number by eliminating the last digit: e.g. 128 /10 = 12
+      // Once we get to a single digit number then it becomes zero: e.g. 1/10 = 0
+      // Faster notation: current_number /= 10;
+      current_number = current_number / 10; // 1pt
+    }
+    
+    // If we have made it through the whole loop without returning, then we 
+    // know it must be a self-divisor
+    return true; // 1pt
+  }
+
 }
--- Trip.java
+++ Trip.java
@@ -3,27 +3,55 @@ import java.util.ArrayList;
 public class Trip {
   private ArrayList<Flight> flights;
 
-  
+// **** Answer to Question 1A ****
+
   public int getDuration () {
-    Time firstDepartureTime = flights.get(0).getDepartureTime();
-    Time lastArrivalTime = flights.get(flights.size()-1).getArrivalTime();
-    return firstDepartureTime.minutesUntil(lastArrivalTime);
+    
+    // Check the size of the Arraylist
+    if (flights.size() == 0) return 0; // 1pt
+
+    // Make a time object to get departure of the first element of the array
+    Time firstDepartureTime = flights.get(0).getDepartureTime();  // 1pt
+
+
+    // Make a time object to get arrival of the last element of the array
+    Time lastArrivalTime = flights.get(flights.size()-1) .getArrivalTime(); // 1pt
+
+    // Find the time between the two Time objects
+    return firstDepartureTime.minutesUntil(lastArrivalTime); // 1pt;
   }
 
-  public int getShortestLayover () {
-    if (flights.size() < 2) return -1;
-    int shortestLayover = Integer.MAX_VALUE;
-    for (int i=0; i < flights.size()-1; i++) {
-       Time arrival = flights.get(i).getArrivalTime();
-       Time nextDeparture = flights.get(i+1).getDepartureTime();
-       int layover = arrival.minutesUntil(nextDeparture);
+// **** Answer to Question 1B ****
 
+  public int getShortestLayover () {
+
+    // Check the size of the ArrayList
+    if (flights.size() < 2) return -1; // 1pt
+
+    // Initialize the shortest layover to be as long as possible
+    int shortestLayover = Integer.MAX_VALUE;
+
+    // Loop to go through each flight in the Arraylist except the last
+    for (int i=0; i < flights.size()-1; i++) {  // 1pt
+
+       // Make a time object to get arrival of the current element of the array
+       Time arrival = flights.get(i).getArrivalTime();
+
+       // Make a time object to get departure of the next element of the array
+       Time nextDeparture = flights.get(i+1).getDepartureTime(); 
+
+       // Find the layover length
+       int layover = arrival.minutesUntil(nextDeparture); // 1pt
+
+       // print out as an error check
        System.out.println(i + ":\tarrival: " + arrival + "\tdeparture: " + nextDeparture + "\tlayover:" + layover );
 
-       if (layover < shortestLayover)
-          shortestLayover = layover;
+        // Compare the layover to the shortest one
+       if (layover < shortestLayover)  // 1/2 print
+          // If its shorter, set the shortest to be equal to the current layover
+          shortestLayover = layover; // 1 pt
     }
-    return shortestLayover;
+    return shortestLayover; // 1/2 pt
   }
 
   // Constructors
